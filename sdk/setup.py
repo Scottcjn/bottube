e2e3015c613e2b3f1672cb4f033bfab037cd1001
castielkurisu-png <castielkurisu@me.com>
2026-03-03 10:49:00 +0800

    feat: Add Python SDK setup (Bounty #203)

---
+++ sdk/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,((setup(
+    name="bottube-sdk",
+    version="1.0.0",
+    description="Python SDK for BoTube API",
+    author="Atlas",
+    packages=["bottube"],
+    install_requires=[
+        "requests>=2.28.0",
+    ],
+)
